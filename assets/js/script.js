27854a4d5df9abb3bd795d388c3c6929a645fd26
Your Name <you@example.com>
2024-06-30 12:42:00 +0200

    cambiamenti html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-// DOM Elements
+// Elementi del DOM
 const display = document.querySelector(".display");
 const buttonMinus = document.querySelector(".button.minus"); 
 const buttonPlus = document.querySelector(".button.plus"); 
 const buttonReset = document.querySelector(".button.reset"); 
 const buttons = document.querySelectorAll(".button");
 
+
 let count = 0;
 let currentColorIndex = 0;
 
-// Update display
+// Aggiornare il display 
 function updateDisplay() {
     display.textContent = count;
 }
 
-// Events to increase / decrease the counter
+// Eventi per aumentare \ decrementare il counter
 buttonPlus.addEventListener("click", incrementCounter);
 buttonMinus.addEventListener("click", decrementCounter);
 buttonReset.addEventListener("click", reset);
@@ -25,8 +26,8 @@ function incrementCounter() {
 
 function decrementCounter() {
     if (count > 0) {
-        count--;
-        updateDisplay();
+    count--;
+    updateDisplay();
     } 
 }
 
@@ -36,13 +37,14 @@ function reset() {
     changeBackgroundColor();
 }
 
-// Events background colors and shadows
+// Eventi per colori background e ombre
+
 const colors = [
-    "#4A6CFC",
-    "#8f8ff2",
-    "#FF9966",
-    "#5EBAA0",
-    "#D797E7"
+ "#4A6CFC",
+ "#8f8ff2",
+"#FF9966",
+"#5EBAA0",
+"#D797E7"
 ];
 
 function changeBackgroundColor() {
@@ -50,14 +52,19 @@ function changeBackgroundColor() {
     const newColor = colors[currentColorIndex];
     const gradient = `linear-gradient(to bottom, ${newColor} 20%, #fff 100%)`;
     document.body.style.background = gradient;
+    document.body.style.display = gradient;
+    document.body.style.title = gradient;
     updateButtonColors(newColor);
+    
 }
 
 function updateButtonColors(color) {
+    const buttons = document.querySelectorAll('.button');
     buttons.forEach(button => {
         button.style.boxShadow = `0 0.2em ${color}`;
     });
 }
 
 changeBackgroundColor();
+
 updateDisplay();
